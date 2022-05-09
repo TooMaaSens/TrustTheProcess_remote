063cfbfaefcc0e4fc49f2bdd314df2835217a18f
Michał <amadeusgiga@gmail.com>
2022-05-09 21:05:36 +0200

    Initial Commit

---
+++ TrustTheProcess/TrustTheProcessApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrustTheProcessApp.swift
+//  TrustTheProcess
+//
+//  Created by Michał Jabłoński on 09/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrustTheProcessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
